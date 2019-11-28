e391bff80a06216b431156e96b74b7f5e28c7703
gichelli <ggonza18@masonlive.gmu.edu>
2019-11-28 18:21:49 -0500

    modified docker-compose

--- src/main/java/com/gichelli/model/Employee.java
+++ src/main/java/com/gichelli/model/Employee.java
@@ -8,9 +8,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.springframework.context.annotation.Configuration;
+
 import lombok.Data;
 
-
+@Configuration
 @Entity
 @Table (name = "employees")
 public class Employee {
--- src/test/java/com/gichelli/test/PosApplicationTests.java
+++ src/test/java/com/gichelli/test/PosApplicationTests.java
@@ -4,6 +4,7 @@
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
@@ -11,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.Test;
 
 
-
+@Configuration
 @RunWith(SpringRunner.class)
   @SpringBootTest 
   public class PosApplicationTests {
