f0476c769fff81ba734d7a53610cf3adc33ca6e9
gichelli <ggonza18@masonlive.gmu.edu>
2019-12-11 21:39:54 -0500

    changing test for pos_jenkins

--- src/test/java/com/gichelli/test/TestPosApp.java
+++ src/test/java/com/gichelli/test/TestPosApp.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -18,13 +19,15 @@ import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest(classes = PosApplicationTests.class)
 
-@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 //@ContextConfiguration(classes = {TestContext.class, WebAppContext.class})
 @WebAppConfiguration
 public class TestPosApp {
 	@Autowired
 	private WebApplicationContext webApplicationContext;
 	
+	@Autowired
 	private MockMvc mockMvc;
 	
 	@Before
